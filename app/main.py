89f18890dfbbc488829507dfbb0b9480925cfb49
Bryan Chuc <bryangcv0308@gmail.com>
2026-07-12 12:02:57 -0500

    Fix CORS middleware configuration

--- app/main.py
+++ app/main.py
@@ -54,7 +54,7 @@ def create_app(settings: Settings | None = None) -> FastAPI:
     app.add_middleware(
         CORSMiddleware,
         # allow_origins=["*"],  
-        allow_origins=["http://localhost:3000"]
+        allow_origins=["http://localhost:3000"],
         allow_credentials=True,
         allow_methods=["*"], 
         allow_headers=["*"], 
